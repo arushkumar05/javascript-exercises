0b3f54323b54ca8840ba6f0875c81fe7cca54e9a
Arush Kumar <arushkumar0510@gmail.com>
2024-07-08 22:11:23 -0400

    Q7 Round off to one decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let cel = 0;
+  cel = ((temp - 32) * 5) / 9;
+  return Math.round(cel * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let fah = 0;
+  fah = (temp * 9) / 5 + 32;
+  return Math.round(fah * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
