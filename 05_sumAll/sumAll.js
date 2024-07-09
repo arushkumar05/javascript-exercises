4517c4bdc1372ee06b7147fda6404607e45e0c09
Arush Kumar <arushkumar0510@gmail.com>
2024-07-08 21:50:39 -0400

    Q5 - typeof operator

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (one, two) {
+  if (one < 0 || two < 0) {
+    return "ERROR";
+  } else if (typeof one != "number" || typeof two != "number") {
+    return "ERROR";
+  } else {
+    let sum = 0;
+    if (one > two) {
+      let temp = two;
+      two = one;
+      one = temp;
+    }
+    for (let i = one; i <= two; i++) {
+      sum += i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
