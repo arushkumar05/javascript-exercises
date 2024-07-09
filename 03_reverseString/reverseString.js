4fc50642b9184b472efb7031b10ff353d6ba4c7a
Arush Kumar <arushkumar0510@gmail.com>
2024-07-08 21:40:00 -0400

    Q4 important three different methods

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (input) {
+  let rev = "";
+  for (let i = input.length - 1; i >= 0; i--) {
+    rev = rev + input.charAt(i);
+  }
+  return rev;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function (input, ...args) {
+  const newArray = [];
+  //   input.forEach((item) => {
+  //     if (!args.includes(item)) {
+  //       newArray.push(item);
+  //     }
+  //   });
+
+  //   for (let i = 0; i < input.length; i++) {
+  //     if (!args.includes(input[i])) {
+  //       newArray.push(input[i]);
+  //     }
+  //   }
+
+  return input.filter((val) => !args.includes(val));
 };
 
 // Do not edit below this line
